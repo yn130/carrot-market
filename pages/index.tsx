cec5fcab52491b24b7e713a6cc49417ff1059bac
yn130 <tjdpdms95@gmail.com>
2025-03-06 16:00:30 +0900

    2.1Test Frive part one

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
-  return <div className="bg-red-500">
-    <h1 className="text-black">it works</h1>
-
-  </div>
+  return (
+    <div className="bg-slate-400 py-20 px-5 grid gap-10">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Tabler</span>
+          <span className="font-semibold">$80</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$99</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">Checkout</div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
+  );
 }
